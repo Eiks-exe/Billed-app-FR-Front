0b3d37c8517dde36330487c1b709dd1281a82602
eiks-exe <eiks_art@outlook.com>
2023-02-10 10:46:50 -0400

    logout_instance

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,14 +2,16 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockstore from "../__mocks__/store"
+import DashboardUI from "../views/DashboardUI"
 
 jest.mock('./Logout', () => jest.fn());
-import Logout from "./Logout.js"
+import Logout from "../containers/Logout.js"
 import Bills from '../containers/Bills.js'
 
 
@@ -43,10 +45,31 @@ describe("Given I am connected as an employee", () => {
   })
 })
 
-describe("Given i am connected as an employee", ()=>{
-  describe('When i am on Bills page and i click on NewBill', ()=>{
-    test(('Then, i should be sent to newBill page'), ()=>{
-      
-    })
-  })
-})
+// describe("Given i am connected as an employee", ()=>{
+//   describe('When i am on Bills page and i click on NewBill', ()=>{
+//     test(('Then, i should be sent to newBill page'), ()=>{
+
+//     })
+//   })
+// })
+
+describe('Given I am connected as an employee', () => {
+  describe('When I click on disconnect button', () => {
+    test('Then, a logout instance should be created ', () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+
+      const logout = new Logout({ document, onNavigate, localStorage });
+      const logoutInitSpy = jest.spyOn(logout, 'init');
+      logout.init();
+
+      expect(logoutInitSpy).toHaveBeenCalled();
+    });
+  });
+});
