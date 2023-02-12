c7a398ed0e746d970aa2f77dea467c07317eb8b7
eiks-exe <eiks_art@outlook.com>
2023-02-12 15:41:48 -0400

    handleiconEye

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -13,7 +13,7 @@ import DashboardUI from "../views/DashboardUI"
 jest.mock('./Logout', () => jest.fn());
 import Logout from "../containers/Logout.js"
 import Bills from '../containers/Bills.js'
-
+import userEvent from "@testing-library/user-event";
 
 import router from "../app/Router.js";
 
@@ -61,7 +61,24 @@ describe("Given I am connected as an employee", () => {
 //   })
 // })
 
+
 describe('Given I am connected as an employee', () => {
+  describe('Given I am connected as an employee', ()=> {
+    test('it should be initialyzed', ()=> {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+  
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }));
+  
+      const bills = new Bills({document, onNavigate, localStorageMock})
+      expect(bills).toBeInstanceOf(Bills);
+    })
+  })
+  
   describe('When I click on disconnect button', () => {
     test('Then, a logout instance should be created ', () => {
       const onNavigate = (pathname) => {
@@ -80,4 +97,156 @@ describe('Given I am connected as an employee', () => {
       expect(logoutInitSpy).toHaveBeenCalled();
     });
   });
+  describe('When I am on Bills Page and i click on NewBill', () => {
+    test(('Then, I should be sent to newBill page'), () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      document.body.innerHTML = BillsUI({ data: bills })
+      const store = null
+      const billsObj = new Bills({ document, onNavigate, store, localStorage })
+      const handleClick = jest.fn(billsObj.handleClickNewBill )
+
+      const newBill = screen.getByTestId('btn-new-bill')
+      newBill.addEventListener('click', handleClick)
+      userEvent.click(newBill)
+      expect(handleClick).toHaveBeenCalled()
+      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
+    })
+  })
+  describe('When I am on Bills Page', () => {
+    test(('Then, I should see a list of bill'), async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      document.body.innerHTML = BillsUI({ data: bills })
+      const store = mockstore;
+      const consoleSpy = jest.spyOn(console, "log");
+      const billsObj = new Bills({ document, onNavigate, store, localStorage })
+      const handleMethod = jest.fn(billsObj.getBills)
+      const result = await handleMethod()
+
+      const billsTable = screen.getByTestId('tbody')
+      const numberOfBill = billsTable.children.length;
+      expect(handleMethod).toHaveBeenCalled()
+      expect(consoleSpy).toHaveBeenCalledWith('length', result.length);
+      expect(bills.length == numberOfBill).toBeTruthy()
+    })
+    
+    describe('When I am on Bills Page', () => {
+      test(('Then, I should see a error'), async () => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        document.body.innerHTML = BillsUI({ data: bills, error: 'errorDetected' })
+        expect(screen.getByTestId('error-message').textContent).toContain('errorDetected');
+      })
+    })
+
+    describe('When I click on a eye icon', () => {
+      test(('Then, I should see a modal opening'), async () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        //Object.defineProperty(window, 'modal', { value: jest.fn })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        document.body.innerHTML = BillsUI({ data: bills })
+        const store = mockstore;
+        const billsObj = new Bills({ document, onNavigate, store, localStorage })
+        const handleClick = jest.fn(billsObj.handleClickIconEye)
+        
+        const iconEye = screen.queryAllByTestId('icon-eye')[0];
+        //@ts-ignore
+        iconEye.addEventListener('click', handleClick(iconEye));
+        
+        userEvent.click(iconEye);
+        //@ts-ignore
+        expect(handleClick).toHaveBeenCalled();
+        await new Promise((r) => setTimeout(r, 1000));
+        expect(screen.getByTestId('modaleFile').classList.contains('show')).toBeTruthy();
+      })
+    })
+  })
 });
+
+
+
+
+// describe('Bills', () => {
+//   let document;
+//   let onNavigate;
+//   let store;
+//   let localStorage;
+//   let bills;
+
+//   beforeEach(() => {
+//     document = {
+//       querySelector: jest.fn(),
+//       querySelectorAll: jest.fn(),
+//     };
+//     onNavigate = jest.fn();
+//     store = {
+//       bills: jest.fn(() => ({
+//         list: jest.fn(() => Promise.resolve([{
+//           date: '2022-01-01',
+//           status: 'paid',
+//         }])),
+//       })),
+//     };
+//     localStorage = {
+//       clear: jest.fn(),
+//       getItem: jest.fn(),
+//       removeItem: jest.fn(),
+//       setItem: jest.fn(),
+//     };
+//     bills = new Bills({ document, onNavigate, store, localStorage });
+//   });
+
+//   it('should initialize', () => {
+//     expect(bills).toBeInstanceOf(Bills);
+//   });
+
+//   it('should initialize the Logout instance', () => {
+//     const logout = require('./Logout');
+//     expect(logout).toHaveBeenCalledWith({ document, localStorage, onNavigate });
+//   });
+
+//   it('should call handleClickNewBill when the button "New Bill" is clicked', () => {
+//     const buttonNewBill = {
+//       addEventListener: jest.fn(),
+//     };
+//     document.querySelector.mockReturnValue(buttonNewBill);
+//     bills = new Bills({ document, onNavigate, store, localStorage });
+//     buttonNewBill.addEventListener.mock.calls[0][1]();
+//     expect(onNavigate).toHaveBeenCalledWith('ROUTES_PATH["NewBill"]');
+//   });
+
+//   it('should call handleClickIconEye when an eye icon is clicked', () => {
+//     const iconEye = [      {        addEventListener: jest.fn(),        getAttribute: jest.fn(() => 'bill-url'),      },    ];
+//     document.querySelectorAll.mockReturnValue(iconEye);
+//     bills = new Bills({ document, onNavigate, store, localStorage });
+//     iconEye[0].addEventListener.mock.calls[0][1]();
+//     expect(iconEye[0].getAttribute).toHaveBeenCalledWith('data-bill-url');
+//     expect(iconEye[0].addEventListener).toHaveBeenCalledWith('click', expect.any(Function));
+//   });
+
+//   it('should get the list of bills', async () => {
+//     const result = await bills.getBills();
+//     expect(result).toEqual([{
+//       date: '2022-01-01',
+//       status: 'Paid',
+//     }]);
+//   });
+// });
