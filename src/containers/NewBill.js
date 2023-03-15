6cd9f3165c869b85175fbd65b09e23c4e3ab009d
eiks-exe <eiks_art@outlook.com>
2023-03-15 11:08:28 -0400

    fix-double-declarations

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,7 +1,5 @@
 import { ROUTES_PATH } from '../constants/routes.js'
 import Logout from "./Logout.js"
-import { ROUTES_PATH } from '../constants/routes.js'
-import Logout from "./Logout.js"
 
 export default class NewBill {
   /**
