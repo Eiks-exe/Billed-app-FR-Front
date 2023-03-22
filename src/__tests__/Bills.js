9c186a01622ce51b43df1d0696b3eafd8cf251bf
eiks-exe <eiks_art@outlook.com>
2023-03-22 12:46:04 -0400

    cleaning comments

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -191,80 +191,10 @@ describe('Given I am connected as an employee', () => {
         expect(handleClick).toHaveBeenCalled();
         await new Promise((r) => setTimeout(r, 1000));
         expect(screen.getByTestId('modaleFile').classList.contains('show')).toBeTruthy();
-        screen.debug(screen.queryAllByTestId('test'))
-        console.log("test")
+        
 
       })
     })
   })
 });
 
-
-
-
-// describe('Bills', () => {
-//   let document;
-//   let onNavigate;
-//   let store;
-//   let localStorage;
-//   let bills;
-
-//   beforeEach(() => {
-//     document = {
-//       querySelector: jest.fn(),
-//       querySelectorAll: jest.fn(),
-//     };
-//     onNavigate = jest.fn();
-//     store = {
-//       bills: jest.fn(() => ({
-//         list: jest.fn(() => Promise.resolve([{
-//           date: '2022-01-01',
-//           status: 'paid',
-//         }])),
-//       })),
-//     };
-//     localStorage = {
-//       clear: jest.fn(),
-//       getItem: jest.fn(),
-//       removeItem: jest.fn(),
-//       setItem: jest.fn(),
-//     };
-//     bills = new Bills({ document, onNavigate, store, localStorage });
-//   });
-
-//   it('should initialize', () => {
-//     expect(bills).toBeInstanceOf(Bills);
-//   });
-
-//   it('should initialize the Logout instance', () => {
-//     const logout = require('./Logout');
-//     expect(logout).toHaveBeenCalledWith({ document, localStorage, onNavigate });
-//   });
-
-//   it('should call handleClickNewBill when the button "New Bill" is clicked', () => {
-//     const buttonNewBill = {
-//       addEventListener: jest.fn(),
-//     };
-//     document.querySelector.mockReturnValue(buttonNewBill);
-//     bills = new Bills({ document, onNavigate, store, localStorage });
-//     buttonNewBill.addEventListener.mock.calls[0][1]();
-//     expect(onNavigate).toHaveBeenCalledWith('ROUTES_PATH["NewBill"]');
-//   });
-
-//   it('should call handleClickIconEye when an eye icon is clicked', () => {
-//     const iconEye = [      {        addEventListener: jest.fn(),        getAttribute: jest.fn(() => 'bill-url'),      },    ];
-//     document.querySelectorAll.mockReturnValue(iconEye);
-//     bills = new Bills({ document, onNavigate, store, localStorage });
-//     iconEye[0].addEventListener.mock.calls[0][1]();
-//     expect(iconEye[0].getAttribute).toHaveBeenCalledWith('data-bill-url');
-//     expect(iconEye[0].addEventListener).toHaveBeenCalledWith('click', expect.any(Function));
-//   });
-
-//   it('should get the list of bills', async () => {
-//     const result = await bills.getBills();
-//     expect(result).toEqual([{
-//       date: '2022-01-01',
-//       status: 'Paid',
-//     }]);
-//   });
-// });
